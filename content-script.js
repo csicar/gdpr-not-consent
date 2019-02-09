d7b673ef2f5a79cd0021fef019c74a81ab15b4ba
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-02-09 23:56:36 +0100

    added domain

---
+++ content-script.js
@@ -0,0 +1,6 @@
+// const banner = document.querySelector('[data-tracking-opt-in-overlay]')
+// if (banner) {
+// 	console.log("banner found. Removing it");
+// 	banner.parentElement.remove()
+// 	document.body.style += (document.body.style ? ";" : "") + "overflow: auto";
+// }
