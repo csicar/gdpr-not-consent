bd48d46d013b39291bc634948e4b891ffa79d596
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-10-05 14:10:35 +0200

    new domains

--- blocked_domains.js
+++ blocked_domains.js
@@ -7,16 +7,24 @@ const domains = [
 
 	//onetrust
 	"https://consent.trustarc.com/*",
-
+	"https://consent-st.trustarc.com/*",
 	"https://jadserve.postrelease.com/**",
-
 	"https://www.googletagmanager.com/gtm.js*",
-
 	"https://admiral.mgr.consensu.org/portal.html",
+	"https://geolocation.onetrust.com/cookieconsentpub*",
 
 
 	"https://*.nocookie.net/*/-/abtesting,oasis_blocking,universal_analytics_js,adengine2_top_js,tracking_opt_in_js,qualaroo_blocking_js",
 
 	//oath 
 	"https://guce.oath.com/collectConsent*",
+
+	// cookiebot
+	"https://consent.cookiebot.com/*",
+
+	//cmp
+	"https://consent.cmp.oath.com/cmp3p.js",
+
+	//fandom
+	"https://origin-images.wikia.com/fandom-ae-assets/tracking-opt-in/*/tracking-opt-in*.js"
 ]
